c77c3ef6cc40f4bbb3194a2c9ddc1051d667df71
deanofwalls <deanofwalls@gmail.com>
2023-12-21 11:51:00 -0500

    Fleshing out class structure for ImageSteganography class

--- src/main/java/com/deanOfWalls/InPlainSight/stenography/ImageStenography.java
+++ src/main/java/com/deanOfWalls/InPlainSight/stenography/ImageStenography.java
@@ -1,4 +1,14 @@
-package com.deanOfWalls.InPlainSight.stenography;
+import java.awt.image.BufferedImage;
 
-public class ImageStenography {
+public class Steganography {
+
+    public static BufferedImage embedSecretImage(BufferedImage decoyImage, byte[] secretImageBytes) {
+        // Implement the logic to embed the secret image bytes into the decoy image
+    }
+
+    public static byte[] extractSecretImage(BufferedImage stegoImage) {
+        // Implement the logic to extract the secret image bytes from the stego image
+    }
+
+    // Additional helper methods as needed
 }
